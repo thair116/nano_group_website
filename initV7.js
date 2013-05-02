579b13073d5a8dbf93cf0ec3cb370778137083c6
Travis Hairfield <twh116@gmail.com>
2013-05-01 22:05:19 -0400

    Site is online!
    Signed-off-by: Travis Hairfield <twh116@gmail.com>

--- initV7.js
+++ initV7.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
 
-
-	$('.last > a').click(function() {
-		return false;
-	});
-
-
-	/*$('.menu-item').hover(function() {
-		$(this).find('.div-sub_nav').stop(true,true).slideDown(2).delay(350);
-	}, function() {
-		$(this).find('.div-sub_nav').slideUp(2);
-	});
-*/
 	var top = $('.access').offset().top;
 	$(window).scroll(function(event) {
 		var y = $(this).scrollTop();
@@ -31,16 +19,6 @@ $(document).ready(function() {
 				}
 			}
 		});
-	
 
-	/*$(window).scroll(function() {
-		var pos = parseFloat($('body').css('margin-left'));
-		/*console.log(pos);*/
-	/*	pos = pos - $(window).scrollLeft();
-	/*console.log($(window).scrollLeft());*/
-	/*	console.log($(window).scrollLeft());
-		$('#access.fixed').css('left', pos + 'px' );
-		
-	});*/
 
 }); 
